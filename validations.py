4ab02f20a281d8f251beea334dffa519d24c2451
alex-omc-ulima <110443246+alex-omc-ulima@users.noreply.github.com>
2022-08-01 23:23:19 -0500

    Update validations.py
    Add more functions from web client

--- validations.py
+++ validations.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+def validate_user(username, minlen):
+  """Checks if the received username matches the required conditions"""
+  # random string that must be deleted by a pull request
+  if type(username) != str:
+    raise TypeError("username MUST be a string")
+    
+  if len(username) < minlen:
+    return False
+  
+  if not username.isalnum():
+    return False
+  
+  #username CAN'T begin with a number
+  if username[0].isnumeric():
+    return False
+  
+  return True
